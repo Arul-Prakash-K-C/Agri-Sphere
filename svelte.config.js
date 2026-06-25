459d41a560f684a4d2cd18ff5cf9f928ab8efc1d
Arul-001 <159093053+Arul-001@users.noreply.github.com>
2026-06-25 15:26:03 +0530

    Over

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+	preprocess: vitePreprocess(),
+
+	compilerOptions: {
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules')
+				? undefined
+				: true
+	},
+
+	kit: {
+		adapter: adapter()
+	}
+};
